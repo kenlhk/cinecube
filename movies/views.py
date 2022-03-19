493385baf7d7aa6e54a3f8bb1097f0f18f0828c6
Ken Leung <kenlhk01@gmail.com>
2022-03-19 13:18:06 +0000

    updated movies app by Dandy

--- movies/views.py
+++ movies/views.py
@@ -12,7 +12,7 @@ def index(request):
     context_dict = {}
     context_dict['title'] = "CineCube"
     context_dict['now_playing'] = Movie.objects.order_by('showing_time')[:5]
-
+    
     return render(request, 'movies/movies_index.html', context=context_dict)
 
 def movie_list(request):
@@ -25,6 +25,17 @@ def movie_list(request):
         context_dict['list'] = None
         
     return render(request, 'movies/movies_list.html', context=context_dict)    
+
+def genre_list(request):
+    context_dict = {}
+    
+    try:
+        genre_list = Category.objects.all()
+        context_dict['list'] = genre_list
+    except Movie.DoesNotExist:
+        context_dict['list'] = None
+        
+    return render(request, 'movies/movies_genres.html', context=context_dict) 
     
 
 def show_movie(request, movie_id_slug):
@@ -37,4 +48,20 @@ def show_movie(request, movie_id_slug):
         context_dict['movie'] = None
     
     return render(request, 'movies/movies_movie.html', context=context_dict)
+
+def show_genre(request, genre_name_slug):
+    context_dict = {}
+    
+    try:
+        genre = Category.objects.get(slug=genre_name_slug)
+        movies = Movie.objects.filter(category=genre)
+        
+        context_dict['category'] = genre
+        context_dict['contents'] = movies
+        
+    except Movie.DoesNotExitst:
+        context_dict['category'] = None
+        context_dict['contents'] = None
+    
+    return render(request, 'movies/movies_genre_page.html', context=context_dict)
 
